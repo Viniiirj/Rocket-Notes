0135a782bf4521c5454dfdb5c91b7505dc315091
viniiirj <vsr.rio@gmail.com>
2023-10-13 20:52:26 -0300

    Criado componente Button

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,9 +1,15 @@
 import { Container } from './styles'
 
+import { Button } from '../../components/Button'
+
 export function Details(){
   return(
   <Container>  
       <h1>Hello, world</h1>
+      <Button title="Entrar" />
+      <Button title="Cadastrar"/>
+      <Button title="Voltar"/>
+
   </Container>
   )
 }
--- rocketnotes/src/pages/styles/global.js
+++ rocketnotes/src/pages/styles/global.js
@@ -5,11 +5,16 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Roboto Slab', serif;
+        font-size: 16px;
+        outline: none;
     }
 
     body {
         background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
         color: ${({ theme }) => theme.COLORS.WHITE};
+
+        -webkit-font-smoothing: antialiased;
 
     }
 
