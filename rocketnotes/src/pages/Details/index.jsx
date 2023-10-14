38749286f6593f538a91a7cc6feef1bef2cbaafa
viniiirj <vsr.rio@gmail.com>
2023-10-13 21:25:42 -0300

    Children

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,12 @@
 import { Container } from './styles'
 
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 
 export function Details(){
   return(
   <Container>  
-      <h1>Hello, world</h1>
-      <Button title="Entrar" />
-      <Button title="Cadastrar"/>
+      <Header />
       <Button title="Voltar"/>
 
   </Container>
