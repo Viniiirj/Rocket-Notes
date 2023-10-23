3108fbbe4591e469a427f3d8a25863fc386e614a
viniiirj <vsr.rio@gmail.com>
2023-10-22 22:11:28 -0300

    TextArea

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,5 +1,7 @@
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
+
 
 import { Container, Form } from './styles';
 
@@ -15,6 +17,8 @@ export function New() {
                     </header>
 
                     <Input placeholder='Título'/>
+
+                    <Textarea placeholder="Observações"/>
                 </Form>
             </main>
         </Container>
